a89f4aa9dbbb150a16967d25938db26e7b38455b
mathesbh <mathesbh@gmail.com>
2021-05-18 13:36:11 -0300

    Adiciona controller questionario post e get

---
+++ Controllers/QuestionnaireController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AtQuiz.Data;
+using AtQuiz.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtQuiz.Controllers
+{
+    [ApiController]
+    [Route("api/questionnaires")]
+    public class QuestionnaireController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Questionnaire>>> Get([FromServices] DataContext context)
+        {           
+            var questionnaires = await context.Questionnaires.ToListAsync();
+            return questionnaires;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Questionnaire>> Post([FromServices] DataContext context, [FromBody] Questionnaire model)
+        {
+            if(ModelState.IsValid)
+            {
+                model.RegistrationDate = DateTime.Now;
+                context.Questionnaires.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
